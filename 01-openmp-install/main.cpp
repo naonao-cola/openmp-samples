1289b617860f071908aa74e25a805a659a3d2e86
lixiaoguang20 <lixiaoguang20@163.com.com>
2024-02-08 22:51:05 +0800

    0208-03

--- 01-openmp-install/main.cpp
+++ 01-openmp-install/main.cpp
@@ -3,10 +3,22 @@
 
 int main() 
 {
-   #pragma omp parallel num_threads(4)
+    //获取cpu核心数量
+    int num_procs = omp_get_num_procs();
+    printf("number procs %d\n", num_procs);
+
+    //获取cpu运行线程数量
+    int num_threads = omp_get_num_threads();
+    printf("number threads %d\n", num_threads);
+
+   #pragma omp parallel num_threads(num_procs*2)
    {
-      int i = omp_get_thread_num();
-      printf("Hello from thread %d\n", i);
+        // //获取cpu运行线程数量
+        // int num_threads = omp_get_num_threads();
+        // printf("number threads %d\n", num_threads);
+
+        int idx = omp_get_thread_num();
+        printf("Hello from thread %d\n", idx);
    }
 
     return 0;
