b80d30ea92eb16dbf796a5a1d5f92ab76a44469b
lixiaoguang <lixiaoguang@example.com>
2024-06-26 14:35:17 +0800

    S2-03-04

--- S2-03-openmp-quicksort/main.cpp
+++ S2-03-openmp-quicksort/main.cpp
@@ -1,81 +1,86 @@
+ 
 #include <iostream>
+#include  <omp.h>
+
 #include"SteadyClock.h"
 
-using namespace std;
-   
-long partition(long arr[], long low, long high) {
-    long pivot = arr[high];
-    long i = (low - 1);
-    
-    for (long j = low; j <= high - 1; j++) {
-        if (arr[j] < pivot) {
-            i++;
-            
-            // 交换 arr[i] 和 arr[j]
-            long temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-    }
-    
-    // 将枢纽元素放置到正确的位置
-    long temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-    
-    return i + 1;
-}
- 
-void quicksort(long arr[], long low, long high) {
-    if (low < high) {
-        // 获取分区后的枢纽位置
-        long pivot_index = partition(arr, low, high);
-        
-        // 分别对枢纽左右两边的子数组进行递归排序
-        quicksort(arr, low, pivot_index - 1);
-        quicksort(arr, pivot_index + 1, high);
-    }
-}
+void quickSort(long *num,long low,long high);//进行分区
+long Partition(long *num,long low,long high);//返回分离点
 
 void fill_random_1d(long n, long *arr) 
 {
     for (long i = 0; i < n; ++i) 
     {
-        arr[i] = rand() % 10;
+        arr[i] = rand();
     }
 }
 
-int main() 
+int main()
 {
     // clock init
     SteadyClock* p_SteadyClock = new SteadyClock();
     p_SteadyClock->Init();
 
     // 创建数据源
-    long n = 1e6;
+    long n = 100;
     long *arr = (long*)malloc(sizeof(long)* n);
 
     // 初始化数据
     fill_random_1d(n, arr);
-        
+
+    //Explicitly disable dynamic teams
+    //omp_set_dynamic(0);             
+
+    //Use N threads for all parallel regions
+    //omp_set_num_threads(16); 
+
     // clock start
     p_SteadyClock->Start();
 
-    quicksort(arr, 0, n - 1);
+	quickSort(arr,0,n-1);
 
     // clock end
     p_SteadyClock->End();
     p_SteadyClock->Duration("quicksort one thread");
-    
-    /*
-    for (long i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-    */
-   
-    // clock release
-    p_SteadyClock->Release();
-    delete p_SteadyClock;
 
-    return 0;
+    
+    for (long i = 0; i < n; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+    
+
+	return 0;
+}
+ 
+ 
+void quickSort(long *num,long low,long high)
+{
+	if(low<high)
+	{
+		long split=Partition(num,low,high);
+        //并行区域
+		{
+            #pragma omp task//负责这个区域的线程对前面部分进行排序
+		    quickSort(num,low,split-1);
+
+            #pragma omp task//负责这个区域的线程对后面部分进行排序
+		    quickSort(num,split+1,high);
+		}
+ 
+	}
+ 
+}
+ 
+long Partition(long *num,long low,long high)
+{
+	long temp=num[low];//作为中轴
+	while(low<high)
+	{
+		while(low<high&&num[high]>=temp)high--;
+		num[low]=num[high];
+		while(low<high&&num[low]<=temp)low++;
+		num[high]=num[low];
+	}
+	num[low]=temp;
+	return low;//返回中轴的位置，再进行分离
 }
